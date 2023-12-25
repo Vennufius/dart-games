a5a5a92ddeb0bfcf38b4bf3b4b1f8e1677a3504f
Verner Villikka <verner.villikka@gmail.com>
2023-12-25 23:51:26 +0200

    made Highscores into a table

--- src/pages/high-segment-scoring.tsx
+++ src/pages/high-segment-scoring.tsx
@@ -1,186 +1,177 @@
 import type { NextPage } from "next";
 import { useState } from "react";
 import { api } from "../utils/api";
+import Highscores from "./highscores";
 
 const HighSegmentScoring: NextPage = () => {
-  const MAX_ROUNDS = 5;
-  const utils = api.useContext();
-  const highSegmentScoringQuery =
-    api.highSegmentScoringRouter.getHighscores.useQuery();
-  const highSegmentScoringCreate =
-    api.highSegmentScoringRouter.create.useMutation({
-      async onSuccess() {
-        await utils.highSegmentScoringRouter.getHighscores.invalidate();
-      },
+    const MAX_ROUNDS = 5;
+    const utils = api.useContext();
+    const highSegmentScoringQuery =
+        api.highSegmentScoringRouter.getHighscores.useQuery();
+    const highSegmentScoringCreate =
+        api.highSegmentScoringRouter.create.useMutation({
+            async onSuccess() {
+                await utils.highSegmentScoringRouter.getHighscores.invalidate();
+            },
+        });
+    const segments = ["20", "19", "18", "Bull"];
+    const [totalScore, setTotalScore] = useState(0);
+    const [round, setRound] = useState(1);
+    const [roundScore, setRoundScore] = useState({
+        dart1: 0,
+        dart2: 0,
+        dart3: 0,
     });
-  const segments = ["20", "19", "18", "Bull"];
-  const [totalScore, setTotalScore] = useState(0);
-  const [round, setRound] = useState(1);
-  const [roundScore, setRoundScore] = useState({
-    dart1: 0,
-    dart2: 0,
-    dart3: 0,
-  });
-  const [dart, setDart] = useState(1);
-  const [segment, setSegment] = useState(0);
-  const [gameOver, setGameOver] = useState(false);
+    const [dart, setDart] = useState(1);
+    const [segment, setSegment] = useState(0);
+    const [gameOver, setGameOver] = useState(false);
 
-  function scoreDart(score: number) {
-    if (dart === 1) {
-      setRoundScore({
-        ...roundScore,
-        dart1: score,
-      });
+    function scoreDart(score: number) {
+        if (dart === 1) {
+            setRoundScore({
+                ...roundScore,
+                dart1: score,
+            });
+        }
+        if (dart === 2) {
+            setRoundScore({
+                ...roundScore,
+                dart2: score,
+            });
+        }
+        if (dart === 3) {
+            setRoundScore({
+                ...roundScore,
+                dart3: score,
+            });
+        }
+        setDart(dart + 1);
+        setTotalScore(totalScore + score);
     }
-    if (dart === 2) {
-      setRoundScore({
-        ...roundScore,
-        dart2: score,
-      });
+
+    function undoDart() {
+        const undoScore = Object.values(roundScore)[dart - 2];
+        if (undoScore !== undefined) {
+            setDart(dart - 1);
+            setTotalScore(totalScore - undoScore);
+            return;
+        }
+        console.error("Round score undefined");
     }
-    if (dart === 3) {
-      setRoundScore({
-        ...roundScore,
-        dart3: score,
-      });
+
+    function nextSegment() {
+        setDart(1);
+        if (segment < segments.length - 1) {
+            setSegment(segment + 1);
+        } else {
+            setSegment(0);
+            if (round === MAX_ROUNDS) {
+                setGameOver(true);
+                highSegmentScoringCreate.mutate(totalScore);
+                return;
+            }
+            setRound(round + 1);
+        }
     }
-    setDart(dart + 1);
-    setTotalScore(totalScore + score);
-  }
 
-  function undoDart() {
-    const undoScore = Object.values(roundScore)[dart - 2];
-    if (undoScore !== undefined) {
-      setDart(dart - 1);
-      setTotalScore(totalScore - undoScore);
-      return;
+    function restartGame() {
+        setDart(1);
+        setRound(1);
+        setSegment(0);
+        setTotalScore(0);
+        setGameOver(false);
     }
-    console.error("Round score undefined");
-  }
 
-  function nextSegment() {
-    setDart(1);
-    if (segment < segments.length - 1) {
-      setSegment(segment + 1);
-    } else {
-      setSegment(0);
-      if (round === MAX_ROUNDS) {
-        setGameOver(true);
-        highSegmentScoringCreate.mutate(totalScore);
-        return;
-      }
-      setRound(round + 1);
-    }
-  }
+    return (
+        <>
+            <div className="flex flex-col gap-12 px-4 py-16">
+                <h1 className="text-2xl font-semibold tracking-tight text-zinc-300 lg:text-5xl">
+                    High Segment Scoring
+                </h1>
 
-  function restartGame() {
-    setDart(1);
-    setRound(1);
-    setSegment(0);
-    setTotalScore(0);
-    setGameOver(false);
-  }
+                <div className="flex w-full justify-center text-xl font-semibold text-white">
+                    {gameOver ? (
+                        <div>
+                            <h1>GAME OVER</h1>
+                            <p>You got {totalScore} points!</p>
+                            <button className="btn" onClick={restartGame}>
+                                Play again!
+                            </button>
+                        </div>
+                    ) : (
+                        <div className="max-w-lg flex-1">
+                            <div className="mb-5">
+                                <div className="lg:text-md flex justify-between text-sm">
+                                    <span>
+                                        Round: {round} / {MAX_ROUNDS}
+                                    </span>
+                                    <span>Score: {totalScore}</span>
+                                </div>
+                                <div className="grid place-content-center p-4 text-center">
+                                    <h3 className="text-sm lg:text-lg">Current Segment</h3>
+                                    <span className="text-5xl lg:text-9xl">
+                                        {segments[segment]}
+                                    </span>
+                                </div>
+                            </div>
+                            <div className="lg:text-md grid grid-cols-3 gap-5 text-center text-sm lg:gap-8">
+                                <div className="">Dart 1</div>
+                                <div>Dart 2</div>
+                                <div>Dart 3</div>
+                                <div>{dart > 1 ? roundScore.dart1 : "-"}</div>
+                                <div>{dart > 2 ? roundScore.dart2 : "-"}</div>
+                                <div>{dart > 3 ? roundScore.dart3 : "-"}</div>
+                                <hr className="col-span-3" />
+                                <button
+                                    className="btn"
+                                    onClick={() => scoreDart(0)}
+                                    disabled={dart > 3}
+                                >
+                                    Miss
+                                </button>
+                                <button
+                                    className="btn"
+                                    onClick={() => scoreDart(1)}
+                                    disabled={dart > 3}
+                                >
+                                    {segments[segment] === "Bull" ? "Bull's ring" : "Single"}
+                                </button>
+                                <button
+                                    className="btn"
+                                    onClick={() => scoreDart(3)}
+                                    disabled={dart > 3}
+                                >
+                                    {segments[segment] === "Bull" ? "Bullseye" : "Trebel"}
+                                </button>
 
-  return (
-    <>
-      <div className="flex flex-col gap-12 px-4 py-16">
-        <h1 className="text-2xl font-semibold tracking-tight text-zinc-300 lg:text-5xl">
-          High Segment Scoring
-        </h1>
-
-        <div className="flex w-full justify-center text-xl font-semibold text-white">
-          {gameOver ? (
-            <div>
-              <h1>GAME OVER</h1>
-              <p>You got {totalScore} points!</p>
-              <button className="btn" onClick={restartGame}>
-                Play again!
-              </button>
-            </div>
-          ) : (
-            <div className="max-w-lg flex-1">
-              <div className="mb-5">
-                <div className="lg:text-md flex justify-between text-sm">
-                  <span>
-                    Round: {round} / {MAX_ROUNDS}
-                  </span>
-                  <span>Score: {totalScore}</span>
+                                <button
+                                    className="btn col-span-3"
+                                    onClick={undoDart}
+                                    disabled={dart <= 1}
+                                >
+                                    Undo
+                                </button>
+                                <button
+                                    className="btn col-span-3"
+                                    onClick={nextSegment}
+                                    disabled={dart < 4}
+                                >
+                                    {round < MAX_ROUNDS
+                                        ? segment < segments.length - 1
+                                            ? "Next Segment"
+                                            : "Next Round"
+                                        : segment < segments.length - 1
+                                            ? "Next Segment"
+                                            : "End Game"}
+                                </button>
+                            </div>
+                        </div>
+                    )}
                 </div>
-                <div className="grid place-content-center p-4 text-center">
-                  <h3 className="text-sm lg:text-lg">Current Segment</h3>
-                  <span className="text-5xl lg:text-9xl">
-                    {segments[segment]}
-                  </span>
-                </div>
-              </div>
-              <div className="lg:text-md grid grid-cols-3 gap-5 text-center text-sm lg:gap-8">
-                <div className="">Dart 1</div>
-                <div>Dart 2</div>
-                <div>Dart 3</div>
-                <div>{dart > 1 ? roundScore.dart1 : "-"}</div>
-                <div>{dart > 2 ? roundScore.dart2 : "-"}</div>
-                <div>{dart > 3 ? roundScore.dart3 : "-"}</div>
-                <hr className="col-span-3" />
-                <button
-                  className="btn"
-                  onClick={() => scoreDart(0)}
-                  disabled={dart > 3}
-                >
-                  Miss
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => scoreDart(1)}
-                  disabled={dart > 3}
-                >
-                  {segments[segment] === "Bull" ? "Bull's ring" : "Single"}
-                </button>
-                <button
-                  className="btn"
-                  onClick={() => scoreDart(3)}
-                  disabled={dart > 3}
-                >
-                  {segments[segment] === "Bull" ? "Bullseye" : "Trebel"}
-                </button>
-
-                <button
-                  className="btn col-span-3"
-                  onClick={undoDart}
-                  disabled={dart <= 1}
-                >
-                  Undo
-                </button>
-                <button
-                  className="btn col-span-3"
-                  onClick={nextSegment}
-                  disabled={dart < 4}
-                >
-                  {round < MAX_ROUNDS
-                    ? segment < segments.length - 1
-                      ? "Next Segment"
-                      : "Next Round"
-                    : segment < segments.length - 1
-                    ? "Next Segment"
-                    : "End Game"}
-                </button>
-              </div>
+                {highSegmentScoringQuery.data && <Highscores scores={highSegmentScoringQuery.data.map(item => ({ id: item.id, username: item.user.name, date: item.createdAt.toLocaleDateString(), score: item.score }))} />}
             </div>
-          )}
-        </div>
-        <div className="grid justify-center text-white">
-          <h1>Highscores:</h1>
-          <ul>
-            {highSegmentScoringQuery.data
-              ?.map((result) => (
-                <li key={result.id}>
-                  {result.user.name} - {result.createdAt.toLocaleDateString()} - {result.score} pts
-                </li>
-              ))}
-          </ul>
-        </div>
-      </div>
-    </>
-  );
+        </>
+    );
 };
 
 export default HighSegmentScoring;
---
+++ src/pages/highscores.tsx
@@ -0,0 +1,36 @@
+type HighscoreProps = {
+    scores: {
+        id: string;
+        username: string;
+        date: string;
+        score: number;
+    }[]
+}
+
+function Highscores({ scores }: HighscoreProps) {
+    return (
+        <div className="text-white">
+            <h1 className="text-2xl mb-5">Highscores:</h1>
+            <table className="w-full border text-sm shadow-sm">
+                <thead className="bg-white/10 text-left">
+                    <tr>
+                        <th className="w-1/3 border border-white/10 p-2">Name</th>
+                        <th className="w-1/3 border border-white/10 p-2">Date</th>
+                        <th className="w-1/3 border border-white/10 p-2">Score</th>
+                    </tr>
+                </thead>
+                <tbody className="text-left">
+                    {scores.map((item) => (
+                        <tr key={item.id}>
+                            <th className="border border-white/10 p-2">{item.username}</th>
+                            <th className="border border-white/10 p-2">{item.date}</th>
+                            <th className="border border-white/10 p-2">{item.score}</th>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    );
+};
+
+export default Highscores;
